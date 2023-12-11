5f0b78c30fb0888d0c1a11101f0e3b7074d25e35
Bence Tóth <bence.toth.work@icloud.com>
2023-12-11 08:58:37 +0100

    Day 04🃏

---
+++ Advent of Code 2023/Day 04/main.swift
@@ -0,0 +1,61 @@
+//
+//  main.swift
+//  Day 04
+//
+//  Created by Bence Tóth on 09/12/2023.
+//
+
+import Foundation
+
+struct Game {
+    var id: Int
+    var matchingNumbers: Int
+    
+    var points: Int {
+        matchingNumbers > 0 ? NSDecimalNumber(decimal: pow(2, matchingNumbers - 1)).intValue : 0
+    }
+    
+    init(_ game: String) {
+        id = Int((game.components(separatedBy: ":").first?.split(separator: " ").last)!)!
+        
+        var winningNumbers = game.components(separatedBy: ":").last?.components(separatedBy: "|").first?.split(separator: " ").map {
+            $0.first == " " ? Int(String($0.dropFirst()))! : Int($0)!
+        }
+        
+        var myNumbers = game.components(separatedBy: ":").last?.components(separatedBy: "|").last?.split(separator: " ").map {
+            $0.first == " " ? Int(String($0.dropFirst()))! : Int($0)!
+        }
+        
+        if let winningNumbers, let myNumbers {
+            matchingNumbers = Set(winningNumbers).intersection(Set(myNumbers)).count
+            return
+        }
+        matchingNumbers = 0
+    }
+}
+
+class Day04: AOCDay {
+    
+    var input: [Game] {
+        FileOpener.shared.openFile(day: "04", test: false).components(separatedBy: "\n").map { Game($0) }
+    }
+    
+    func partOne() -> Int {
+        return input.map(\.points).reduce(0, +)
+    }
+    
+    func partTwo() -> Int {
+        var copies = [Int](repeating: 1, count: input.count + 1)
+        copies[0] = 0
+        
+        for card in input where card.matchingNumbers > 0 {
+            for i in card.id + 1 ... card.id + card.matchingNumbers {
+                copies[i] += copies[card.id]
+            }
+        }
+        return copies.reduce(0, +)
+    }
+}
+
+print(Day04().partOne())
+print(Day04().partTwo())
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 ![Last Commit on GitHub](https://img.shields.io/github/last-commit/bence-t0th/Advent-of-Code-2023)
 
-# :christmas_tree: Advent of Code 2023 :christmas_tree:
+# Advent of Code 2023
 
 <img src="advent2023.png" style="width: 100%;"/>
 
 | Day | Stars |
 | ------ | ------ |
 | [Day 1: Trebuchet?!](https://adventofcode.com/2023/day/1) | :star: :star: |
-| [Day 2: Day 2: Cube Conundrum](https://adventofcode.com/2023/day/2) | :star: :star: |
+| [Day 2: Cube Conundrum](https://adventofcode.com/2023/day/2) | :star: :star: |
+| [Day 4: Scratchcards](https://adventofcode.com/2023/day/4) | :star: :star: |
